ab43116925cd177ac13e45585e10000f35c5d7bf
wxwong0019 <45294269+wxwong0019@users.noreply.github.com>
2021-10-13 16:21:28 -0700

    Update timersAction.js

--- src/store/actions/timersAction.js
+++ src/store/actions/timersAction.js
@@ -1,27 +1,34 @@
 export const addTimer = (startTime) => {
-    return (dispatch) => {
-      dispatch({
-        type: 'addTimer',
-        payload: startTime,
-      });
-    };
+  return (dispatch) => {
+    dispatch({
+      type: 'addTimer',
+      payload: startTime,
+    });
   };
-  
-  export const removeTimer = (position) => {
-    return (dispatch) => {
-      dispatch({
-        type: 'removeTimer',
-        payload: position,
-      });
-    };
+};
+
+export const removeTimer = (position) => {
+  return (dispatch) => {
+    dispatch({
+      type: 'removeTimer',
+      payload: position,
+    });
   };
-  
-  export const updateTimer = (position) => {
-    return (dispatch) => {
-      dispatch({
-        type: 'updateTimer',
-        payload: position,
-      });
-    };
+};
+
+export const updateTimer = (position, t) => {
+  return (dispatch) => {
+    dispatch({
+      type: 'updateTimer',
+      payload: { position: position, time: t },
+    });
   };
-  
+};
+export const sortTimer = () => {
+  return (dispatch) => {
+    dispatch({
+      type: 'sortTimer',
+      payload: null,
+    });
+  };
+};
