77ed250251feca8e4b10c6189165e9a246a6e686
wxwong0019 <45294269+wxwong0019@users.noreply.github.com>
2021-10-13 16:21:58 -0700

    Update timersReducer.js

--- src/store/reducers/timersReducer.js
+++ src/store/reducers/timersReducer.js
@@ -1,44 +1,55 @@
 const timersReducer = (state = [], action) => {
-    switch (action.type) {
-      case 'addTimer':
-        console.log('addTimer :' + action.payload.startTime);
-        var newState = [...state];
-        if (newState.length === 0) {
-          console.log("newState.length === 0")
-          newState.push(action.payload);
-          return newState;
-        }
-        // newState.splice(0, 0, action.payload);
-        for (let i = 0; i < newState.length; i++) {
-            console.log("entered forLoop")
-          if (newState[i] === null){
-            console.log("entered continue")
-            continue;
-          } 
-          if (action.payload.startTime <= newState[i].startTime) {
-            console.log("splice :"+action.payload.startTime+" to :"+newState[i].startTime)
-            newState.splice(i, 0, action.payload);
-            break;
-          }
-        }
-        if (
-          newState[newState.length - 1] === null ||
-          action.payload.startTime > newState[newState.length - 1].startTime
-        ) {
-            console.log("action.payload.startTime > newState[newState.length - 1].startTime? :"+ (action.payload.startTime>newState[newState.length - 1].startTime))
-          newState.push(action.payload);
-        }
-  
+  switch (action.type) {
+    case 'addTimer':
+      console.log('addTimer :' + typeof state);
+      var newState = [...state];
+      if (newState.length === 0) {
+        newState.push(action.payload);
         return newState;
-      case 'removeTimer':
-        console.log('removeTimer :' + action.payload);
-        var newState = [...state];
-        newState.splice(action.payload, 1, null);
-        return newState;
-      default:
-        return state;
-    }
-  };
-  
-  export default timersReducer;
-  
+      }
+      // newState.splice(0, 0, action.payload);
+      for (let i = 0; i < newState.length; i++) {
+        if (newState[i] === null) continue;
+        if (action.payload.startTime <= newState[i].updateTime) {
+          newState.splice(i, 0, action.payload);
+          break;
+        }
+      }
+      if (
+        newState[newState.length - 1] === null ||
+        action.payload.startTime > newState[newState.length - 1].updateTime
+      ) {
+        newState.push(action.payload);
+      }
+
+      return newState;
+    case 'removeTimer':
+      console.log('removeTimer :' + action.payload);
+      var newState = [...state];
+      newState.splice(action.payload, 1, null);
+      return newState;
+    case 'updateTimer':
+      console.log(
+        'updateTimer :' + action.payload.position + ' ' + action.payload.time
+      );
+      var newState = [...state];
+      if (newState[action.payload.position] !== null) {
+        newState[action.payload.position].updateTime = action.payload.time;
+      }
+      return newState;
+    case 'sortTimer':
+      var newState = [...state];
+      if (newState.length > 1) {
+        newState.sort((a, b) => {
+          console.log('a :' + a.updateTime + ' b :' + b.updateTime);
+          return a.updateTime - b.updateTime;
+        });
+      }
+
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default timersReducer;
